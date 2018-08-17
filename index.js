cfc9072fe35323c9481b9859bb31d8d4c02711b8
Alison Kanegae <alisonkanegae@gmail.com>
2018-08-17 02:23:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // write your code below!
 console.log("Hello, world!")
+var dog = "cuddly"
+
+if (dog === "hungry") {
+  console.log("Refilling food bowl")
+} else if (dog === "thirsty") {
+  console.log("Refilling water bowl")
+} else if (dog === "cuddly") {
+  console.log("Snuggling")
+} else if (dog === "bored") {
+  console.log(dog === "playing")
+} else {
+  console.log("Reading newspaper")
+}
